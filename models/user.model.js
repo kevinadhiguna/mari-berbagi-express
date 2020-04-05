3db6e4732f9f29f58a04cbdc5775b616a7ce5c2f
Firmansyah Yanuar <fyfirman@gmail.com>
2020-04-06 00:52:13 +0700

    refactor user schema

--- models/user.model.js
+++ models/user.model.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-let User = new Schema({
-    _id: { type: String },
-    email: { type: String },
-    password: { type: String },
+
+const Profile = new Schema({
     name: { type: String },
     address: { type: String },
     profile_picture: { type: String },
     phone: { type: String },
+    email: { type: String }
 });
+
+const User = new Schema({
+    _id: { type: String },
+    username: { type: String },
+    password: { type: String },
+    profile: [Profile]
+});
+
 module.exports = mongoose.model('User', User);
