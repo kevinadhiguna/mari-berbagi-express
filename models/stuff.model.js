04304f6d6bf2088bbe430a8e4f0c1e78a549ce09
Firmansyah Yanuar <fyfirman@gmail.com>
2020-04-06 00:52:27 +0700

    create stuff schema

---
+++ models/stuff.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Stuff = new Schema({
+    _id: { type: String },
+    name: { type: String },
+    description: { type: String },
+    picture: { type: String },
+    status: { type: String },
+    amount: { type: String },
+    condition: { type: String },
+    address: { type: String },
+    is_cod: { type: Boolean },
+    postal_fee: { type: String },
+    user_id: { type: Schema.Types.ObjectId, ref: 'User' },
+    timestamps: true
+});
+
+module.exports = mongoose.model('Stuff', Stuff);
