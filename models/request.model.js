871095a1d200bdc18d5d12488bfb61058a15991d
Firmansyah Yanuar <fyfirman@gmail.com>
2020-04-06 01:00:22 +0700

    create request schema

---
+++ models/request.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Request = new Schema({
+    _id: { type: String },
+    name: { type: String },
+    reason: { type: String },
+    amount: { type: String },
+    address: { type: String },
+    is_cod: { type: Boolean },
+    postal_fee: { type: String },
+    status: {
+        type: String,
+        enum: ['Paid', 'Not yet paid', 'Waiting', 'Rejected']
+    },
+    user_id: { type: Schema.Types.ObjectId, ref: 'User' },
+    stuff_id: { type: Schema.Types.ObjectId, ref: 'Stuff' },
+    timestamps: true
+});
+
+module.exports = mongoose.model('Request', Request);
